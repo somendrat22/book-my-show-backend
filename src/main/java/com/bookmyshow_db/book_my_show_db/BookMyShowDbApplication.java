08f77ba562c8b0bd9de178370b3e92d785fa2ace
Somendrat22 <somendrat22@gmail.com>
2025-01-15 20:52:10 +0530

    DBAPI: Initial Code ready

---
+++ src/main/java/com/bookmyshow_db/book_my_show_db/BookMyShowDbApplication.java
@@ -0,0 +1,13 @@
+package com.bookmyshow_db.book_my_show_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookmyshow_db/book_my_show_db/BookMyShowDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmyshow_db.book_my_show_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
