0ba3bd1365b7189d00e16cc6a6f3f8bd67e63a51
Somendrat22 <somendrat22@gmail.com>
2025-01-15 21:47:08 +0530

    Mail: Initial code

---
+++ src/main/java/com/bookmyshow_mail/book_my_show_mail/BookMyShowMailApplication.java
@@ -0,0 +1,13 @@
+package com.bookmyshow_mail.book_my_show_mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookmyshow_mail/book_my_show_mail/BookMyShowMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmyshow_mail.book_my_show_mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
